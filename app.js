5c0e6b84c500a61bb7391764dfe0d8116b4cf6c2
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2015-06-20 12:01:57 -0400

    Version .01

--- app.js
+++ app.js
@@ -2,21 +2,39 @@ var charName;
 var charDepartment;
 var player1;
 var player2;
-var player1Turn = true;
+var turnCounter = 1;
 $('#gamePage').hide();
+$('#winLoseBlock').hide();
+
+$('#actionAbility1Block').hover(function(e) {
+  e.preventDefault();
+  $('#infoBoxBlockTitle').text(player1.ability1.title);
+  $('#infoBoxBlockCost span').text(player1.ability1.cost);
+  $('#infoBoxBlockDescription').text(player1.ability1.description);
+  $('#infoBoxBlock').show();
+}, function(e) {
+  $('#infoBoxBlock').hide();
+});
+
+$('#actionAbility2Block').hover(function(e) {
+  e.preventDefault();
+  $('#infoBoxBlockTitle').text(player1.ability2.title);
+  $('#infoBoxBlockCost span').text(player1.ability2.cost);
+  $('#infoBoxBlockDescription').text(player1.ability2.description);
+  $('#infoBoxBlock').show();
+}, function(e) {
+  $('#infoBoxBlock').hide();
+});
 
 $('.creationDepartmentBlock').on('click', function(e) {
   $(this).siblings().removeClass('activeDepartment');
   $(this).addClass('activeDepartment');
   var charDepartmentTitle = $(this).find('.creationDepartmentName').text().trim();
-  console.log(charDepartmentTitle);
   _.each(departments, function(el, idx, arr) {
     if (departments[idx].title === charDepartmentTitle) {
       charDepartment = el;
     }
   });
-
-  console.log(charDepartment);
 });
 
 $('#creationSubmitButton').on('click', function(e) {
@@ -26,16 +44,221 @@ $('#creationSubmitButton').on('click', function(e) {
     var enemyInfo = setUpEnemy();
     player1 = new Character(charInfo);
     player2 = new Character(enemyInfo);
-    console.log(player2);
     $('#characterNameInput').val("");
     $('.creationDepartmentBlock').removeClass('activeDepartment');
     $('#characterCreationPage').fadeOut();
     $('#gamePage').fadeIn();
     setUpStats();
+    whoseTurn();
   } else {
 
   }
 });
+
+$('#actionAttackBlock').on('click', function(e) {
+  e.preventDefault();
+  console.log('TURNCOUNT: ', turnCounter);
+  attackAction();
+  setTimeout(enemyAction, 2000);
+  console.log('TURNCOUNT: ', turnCounter);
+  whoseTurn();
+});
+
+$('#actionAbility1Block').on('click', function(e) {
+  e.preventDefault();
+  if (player1.energy >= player1.ability1.cost) {
+    console.log('TURNCOUNT: ', turnCounter);
+    ability1Action();
+    setTimeout(enemyAction, 2000);
+    console.log('TURNCOUNT: ', turnCounter);
+    whoseTurn();
+  } else {
+    $('#combatLogText').text("Not enough energy!");
+    $('#combatLogText').fadeIn().delay(1000).fadeOut();
+  }
+});
+
+$('#actionAbility2Block').on('click', function(e) {
+  e.preventDefault();
+  if (player1.energy >= player1.ability1.cost) {
+    console.log('TURNCOUNT: ', turnCounter);
+    ability2Action();
+    setTimeout(enemyAction, 2000);
+    console.log('TURNCOUNT: ', turnCounter);
+    whoseTurn();
+  } else {
+    $('#combatLogText').text("Not enough energy!");
+    $('#combatLogText').fadeIn().delay(1000).fadeOut();
+  }
+});
+
+$('#winLosePlayAgain').on('click', function(e) {
+  e.preventDefault();
+  $('#winLoseBlock').hide();
+  $('#gamePage').hide();
+  $('#characterCreationPage').fadeIn();
+})
+
+function enemyAction() {
+  var enemyActionNumber = Math.floor(Math.random() * 4) + 1;
+  if (enemyActionNumber === 1 || enemyActionNumber === 2) {
+    attackAction();
+    whoseTurn();
+  } else if (enemyActionNumber === 3) {
+    ability1Action();
+    whoseTurn();
+  } else {
+    ability2Action();
+    whoseTurn();
+  }
+}
+
+function combatLogAttack(damageDealt) {
+  if (turnCounter % 2 !== 0) {
+    console.log('Player 1 is attacking.');
+    $('#combatLogText').text(player1.name + " attacked " + player2.name + " for " + damageDealt + " damage!");
+    $('#combatLogText').fadeIn().delay(1000).fadeOut();
+  } else {
+    console.log('Player 2 is attacking.');
+    $('#combatLogText').text(player2.name + " attacked " + player1.name + " for " + damageDealt + " damage!");
+    $('#combatLogText').fadeIn().delay(1000).fadeOut();
+  }
+}
+
+function attackAction() {
+  if (turnCounter % 2 !== 0) {
+    var damageDealt = Math.round((player1.attack - (0.60 * player2.defense)));
+    player2.health -= damageDealt;
+    setUpStats();
+    combatLogAttack(damageDealt);
+    player2DeathCheck();
+    if (player2DeathCheck() === false) {
+      turnCounter += 1;
+    }
+  } else {
+    var damageDealt = Math.round((player2.attack - (0.60 * player1.defense)));
+    player1.health -= damageDealt;
+    setUpStats();
+    combatLogAttack(damageDealt);
+    player1DeathCheck();
+    if (player1DeathCheck() === false) {
+      turnCounter += 1;
+    }
+  }
+}
+
+function ability1Action() {
+  console.log(player1.energy);
+  console.log(player1.ability1.cost);
+  if (turnCounter % 2 !== 0) {
+    if (player1.energy >= player1.ability1.cost) {
+      player1.ability1.player1Action();
+      setUpStats();
+      player2DeathCheck();
+      if (player2DeathCheck() === false) {
+        turnCounter += 1;
+      }
+    }
+  } else {
+    if (player2.energy >= player2.ability1.cost) {
+      player2.ability1.player2Action();
+      setUpStats();
+      player1DeathCheck();
+      if (player1DeathCheck() === false) {
+        turnCounter += 1;
+      }
+    } else {
+      enemyAction();
+    }
+  }
+}
+
+function ability2Action() {
+  console.log('ENERGY BEFORE: ', player1.energy);
+  console.log('COST BEFORE: ', player1.ability2.cost);
+  if (turnCounter % 2 !== 0) {
+    if (player1.energy >= player1.ability2.cost) {
+      player1.ability2.player1Action();
+      console.log('ENERGY AFTER: ', player1.energy);
+      console.log('COST AFTER: ', player1.ability2.cost);
+      setUpStats();
+      player2DeathCheck();
+      if (player2DeathCheck() === false) {
+        turnCounter += 1;
+      }
+    }
+  } else {
+    if (player2.energy >= player2.ability2.cost) {
+      player2.ability2.player2Action();
+      setUpStats();
+      player1DeathCheck();
+      if (player1DeathCheck() === false) {
+        turnCounter += 1;
+      }
+    } else {
+      enemyAction();
+    }
+  }
+}
+
+function player1DeathCheck() {
+  if (player1.health <= 0) {
+    $('#winLoseText').text("Your loser!");
+    $('#winLoseBlock').fadeIn();
+    resetGame();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function player2DeathCheck() {
+  if (player2.health <= 0) {
+    $('#winLoseText').text("Your Winnar!");
+    $('#winLoseBlock').fadeIn();
+    resetGame();
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function whoseTurn() {
+  var turnAnnouncement;
+  if (turnCounter % 2 !== 0) {
+    turnAnnouncement = player1.name + "'s turn.";
+    $('#turnBlock').text(turnAnnouncement);
+    if (player1.energy < player1.maxEnergy) {
+      if ((player1.energy += 10) >= player1.maxEnergy) {
+        player1.energy = player1.maxEnergy;
+        setUpStats();
+      } else {
+        player1.energy += 10;
+        setUpStats();
+      }
+    }
+  } else {
+    turnAnnouncement = player2.name + "'s turn.";
+    $('#turnBlock').text(turnAnnouncement);
+    if (player2.energy < player2.maxEnergy) {
+      if ((player2.energy += 10) >= player2.maxEnergy) {
+        player2.energy = player2.maxEnergy;
+        setUpStats();
+      } else {
+        player2.energy += 10;
+        setUpStats();
+      }
+    }
+  }
+}
+
+function resetGame() {
+  charName;
+  charDepartment;
+  player1;
+  player2;
+  turnCounter = 1;
+}
 
 function Character(charInfo) {
   this.name = charInfo.name;
@@ -86,18 +309,28 @@ function setUpEnemy() {
 
 function setUpStats() {
   $('#playerInfoTitle h3').text(player1.name + " from " + player1.department);
-  $('#playerHealth').text(player1.health);
-  $('#playerEnergy').text(player1.energy);
-  $('#playerAttack').text(player1.attack);
-  $('#playerDefense').text(player1.defense);
+  $('#playerHealth').text(Math.round(player1.health));
+  if (player1.energy <= 0) {
+    player1.energy = 0;
+    $('#playerEnergy').text(0)
+  } else {
+    $('#playerEnergy').text(Math.round(player1.energy));
+  }
+  $('#playerAttack').text(Math.round(player1.attack));
+  $('#playerDefense').text(Math.round(player1.defense));
   $('#playerIcon').html(player1.icon);
   $('#actionAbility1Block').text(player1.ability1.title);
   $('#actionAbility2Block').text(player1.ability2.title);
 
   $('#enemyInfoTitle h3').text(player2.name + " from " + player2.department);
-  $('#enemyHealth').text(player2.health);
-  $('#enemyEnergy').text(player2.energy);
-  $('#enemyAttack').text(player2.attack);
-  $('#enemyDefense').text(player2.defense);
+  $('#enemyHealth').text(Math.round(player2.health));
+  if (player2.energy <= 0) {
+    player2.energy = 0;
+    $('#enemyEnergy').text(0)
+  } else {
+    $('#enemyEnergy').text(Math.round(player2.energy));
+  }
+  $('#enemyAttack').text(Math.round(player2.attack));
+  $('#enemyDefense').text(Math.round(player2.defense));
   $('#enemyIcon').html(player2.icon);
 }
--- data.js
+++ data.js
@@ -13,15 +13,41 @@ var departments = [
     abilities: [
       {
         title: "Number Crunch",
-        cost: 45,
-        power: 40,
-        description: "Deals 40 damage to your enemy."
+        cost: 40,
+        description: "Deals 25 damage to your enemy, and reduces their attack by 10%.",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability1.title + " and dealt 25 damage to " + player2.name + ", reducing their attack by 10%.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability1.cost;
+          player2.health -= 25;
+          player2.attack -= (player2.attack * 0.1);
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability1.title + " and dealt 25 damage to " + player1.name + ", reducing their attack by 10%.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability1.cost;
+          player1.health -= 25;
+          player1.attack -= (player1.attack * 0.1);
+        }
       },
       {
         title: "Bankruptcy",
         cost: 60,
-        power: 1.4,
-        description: "Deals 20 damage to your enemy and returns it as health."
+        description: "Deals 20 damage to your enemy and returns it as health.",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability2.title + " and drained 20 health from " + player2.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability2.cost;
+          player2.health -= 20;
+          player1.health += 20;
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability2.title + " and drained 20 health from " + player1.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability2.cost;
+          player1.health -= 20;
+          player2.health += 20;
+        }
       }
     ]
   },
@@ -35,15 +61,51 @@ var departments = [
     abilities: [
       {
         title: "Micromanage",
-        cost: 45,
-        power: 40,
-        description: "Deals damage equal to 30% of your enemy's health."
+        cost: 50,
+        description: "Deals damage equal to 30% of your enemy's health.",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability1.title + " and dealt " + (0.30 * player2.health) + " damage to " + player2.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability1.cost;
+          player2.health -= (0.30 * player2.health);
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability1.title + " and dealt " + (0.30 * player1.health) + " damage to " + player1.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability1.cost;
+          player1.health -= (0.30 * player1.health);
+        }
       },
       {
         title: "Fire",
-        cost: 60,
-        power: 1.4,
-        description: "Has a 50% chance to deal 60 damage."
+        cost: 50,
+        description: "Has a 50% chance to deal 60 damage.",
+        player1Action: function() {
+          var fireChance = Math.floor(Math.random() * 2) + 1;
+          if (fireChance === 2) {
+            $('#combatLogText').text(player1.name + " used " + player1.ability2.title + " and dealt 60 damage to " + player2.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player1.energy -= player1.ability2.cost;
+            player2.health -= 60;
+          } else {
+            $('#combatLogText').text(player1.name + " used " + player1.ability2.title + " but it missed!");
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player1.energy -= player1.ability2.cost;
+          }
+        },
+        player2Action: function() {
+          var fireChance = Math.floor(Math.random() * 2) + 1;
+          if (fireChance === 2) {
+            $('#combatLogText').text(player2.name + " used " + player2.ability2.title + " and dealt 60 damage to " + player1.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player2.energy -= player2.ability2.cost;
+            player1.health -= 60;
+          } else {
+            $('#combatLogText').text(player2.name + " used " + player2.ability2.title + " but it missed!");
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player2.energy -= player2.ability2.cost;
+          }
+        }
       }
     ]
   },
@@ -58,14 +120,38 @@ var departments = [
       {
         title: "Advertise",
         cost: 45,
-        power: 40,
-        description: "Deals 25 damage as well as boosting your defense by 30%."
+        description: "Deals 25 damage and increases your defense by 10%.",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability1.title + " and dealt 25 damage to " + player2.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability1.cost;
+          player2.health -= 25;
+          player1.defense = (player1.defense * 1.1);
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability1.title + " and dealt 25 damage to " + player1.name);
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability1.cost;
+          player1.health -= 25;
+          player2.defense = (player1.defense * 1.1);
+        }
       },
       {
         title: "Broadcast",
         cost: 60,
-        power: 1.4,
-        description: "Heals self by 40."
+        description: "Heals self by 40.",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability2.title + ", regaining 40 health.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability2.cost;
+          player1.health += 40;
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability2.title + ", regaining 40 health.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability2.cost;
+          player2.health += 40;
+        }
       }
     ]
   },
@@ -79,15 +165,53 @@ var departments = [
     abilities: [
       {
         title: "Cold-Call",
-        cost: 45,
-        power: 40,
-        description: "Deals 30 damage and reduces your enemy's defense by 30%"
+        cost: 50,
+        description: "Deals 30 damage and reduces your enemy's defense by 10%",
+        player1Action: function() {
+          $('#combatLogText').text(player1.name + " used " + player1.ability1.title + " and dealt 30 damage to " + player2.name + ", reducing his defense by 10%.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player1.energy -= player1.ability1.cost;
+          player2.health -= 30;
+          player2.defense -= (player2.defense * 0.1);
+        },
+        player2Action: function() {
+          $('#combatLogText').text(player2.name + " used " + player2.ability1.title + " and dealt 30 damage to " + player1.name + ", reducing his defense by 10%.");
+          $('#combatLogText').fadeIn().delay(1000).fadeOut();
+          player2.energy -= player2.ability1.cost;
+          player1.health -= 30;
+          player1.defense -= (player1.defense * 0.1);
+        }
       },
       {
         title: "Close Sale",
         cost: 60,
-        power: 1.4,
-        description: "Starts at 30 damage, deals more if you have low health."
+        description: "Instantly kills an enemy 25% health or less, otherwise deals 30 damage.",
+        player1Action: function() {
+          if (player2.health <= (0.25 * player2.maxHealth)) {
+            $('#combatLogText').text(player1.name + " used " + player1.ability2.title + " instantly killing " + player2.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player1.energy -= player1.ability1.cost;
+            player2.health -= (0.25 * player2.maxHealth);
+          } else {
+            $('#combatLogText').text(player1.name + " used " + player1.ability2.title + " and dealt 30 damage to " + player2.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player1.energy -= player1.ability2.cost;
+            player2.health -= 30;
+          }
+        },
+        player2Action: function() {
+          if (player1.health <= (0.25 * player1.maxHealth)) {
+            $('#combatLogText').text(player2.name + " used " + player2.ability2.title + " instantly killing " + player1.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player2.energy -= player2.ability2.cost;
+            player1.health -= (0.25 * player1.maxHealth);
+          } else {
+            $('#combatLogText').text(player2.name + " used " + player2.ability2.title + " and dealt 30 damage to " + player1.name);
+            $('#combatLogText').fadeIn().delay(1000).fadeOut();
+            player2.energy -= player2.ability2.cost;
+            player1.health -= 30;
+          }
+        }
       }
     ]
   }
